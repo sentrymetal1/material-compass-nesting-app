259f3b630b67f8f97cf26639b1668baabc4488a7
sentrymetal1 <sentrymetal1@gmail.com>
2026-03-14 21:49:14 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 const ZOHO = {
   clientId: process.env.ZOHO_CLIENT_ID,
   clientSecret: process.env.ZOHO_CLIENT_SECRET,
-  refreshToken: process.env.ZOHO_REFRESH_TOKEN,
+  refreshToken: process.env.ZOHO_REFRESH_TOKEN,1000.f0b270455a2b4ec7ba3007e43ad90cfc.fb7108bee940bc03ecaa469fd9ef08ae
   accountOwner: process.env.ZOHO_ACCOUNT_OWNER || 'mark_sentrymetal',
   appLinkName: process.env.ZOHO_APP_LINK_NAME || 'type-formsheet-2-18-21',
 };
